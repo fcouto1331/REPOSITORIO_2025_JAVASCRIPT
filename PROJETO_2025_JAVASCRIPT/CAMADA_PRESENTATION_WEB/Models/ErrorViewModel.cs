e7ba946f9c6af77fbf4527d13dd6e611c9b52525
Flávio Couto <flavio.couto@live.com>
2025-05-09 19:06:17 -0300

    TASK BEGIN

---
+++ PROJETO_2025_JAVASCRIPT/CAMADA_PRESENTATION_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CAMADA_PRESENTATION_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
